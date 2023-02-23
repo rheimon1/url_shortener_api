6f701eee7dbbe824806383cea95003b7d1fb874e
Wallace Soares <wallace.soares@pontaltech.com.br>
2023-02-23 10:24:07 -0300

    chore: improve package.json and add any files to .env

--- dist/domain/usecases/shorten-url.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.response = void 0;
-const response = (success, data) => ({
-    success,
-    data
-});
-exports.response = response;
-//# sourceMappingURL=shorten-url.js.map
